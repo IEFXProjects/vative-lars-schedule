be3579d01091ba00f94c08a98dbfbab23c9820a5
Jacob Sanchez <sanchezjacob244@gmail.com>
2015-07-28 09:22:36 -0500

    This is the updated homepage

--- FreeTime.php
+++ FreeTime.php
@@ -8,18 +8,19 @@
 	$chooseMH=$_Post['chooseMH'];
 
 	$count=0;
+	$Act= array();
 	while($count<=$chooseActnum){
-		$Act[$count]=array ($_POST['"Act" . ($count+1)'], $_POST['"time" . ($count+1)';
-		$count=$count+1;
+		$Act[$count]=array($_POST['"Act" . ($count+1)'], $_POST['"time" . ($count+1)'];
+		$count=($count+1);
 	}
-	require 'functions2.php';
-	sessionpage();
-	retrieveUserInfo();
-	$Act = mysqli_real_escape_string($conn, $Act);
-	//converts form data to variables
 	
-	$servername = "localhost";
-	$DBusername = "cl29-vative";
-	$DBpassword = "f4V-NrKV8";
-	$DBname = "cl29-vative";
+	$Act = mysqli_real_escape_string($conn2, $Act);
+	$sql="INSERT INTO Events(Act)" VALUES('$Act');
+	if ($conn2->query($sql)===TRUE){
+	header("Location:https://web125.secure-secure.co.uk/peerphinder.com/schedule/home.html");
+}
+	else{
+	die("Connection failed dummy" . $conn2->connect_error);
+}
+	//converts form data to variables
 	mysqli_close($conn2);
